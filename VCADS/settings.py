a4f115c569eed157260adcdb1f52591bf414d30b
zafranZS <muhammad.zafran@outlook.com>
2018-03-06 09:57:05 +0800

    deploy 10

--- VCADS/settings.py
+++ VCADS/settings.py
@@ -14,12 +14,11 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 ALLOWED_HOSTS = ['*']
 
@@ -84,10 +83,10 @@ WSGI_APPLICATION = 'VCADS.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'vcads',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': '127.0.0.1',
+        'NAME': 'dar9guckjp8e8m',
+        'USER': 'zcshamogbrbtef',
+        'PASSWORD': '2bd237820b833bbff44f13b40a3fe34ac4f00b5020e2315aa9844d876adfcc36',
+        'HOST': 'ec2-54-204-46-60.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
@@ -130,9 +129,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
