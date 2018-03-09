24dd62d9695254f23ea36458879fa836df994b90
zafranZS <muhammad.zafran@outlook.com>
2018-03-09 12:36:32 +0800

    vehicle api 2

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='user_view'),
-    url(r'^api/model/autocomplete', name='vehicle_model_auto_complete')
+    url(r'^$', views.index, name='user_view')
 ]
--- vehicle/urls.py
+++ vehicle/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='user_view')
+    url(r'^$', views.index, name='user_view'),
+    url(r'^api/model/autocomplete', views.VehicleModelAutoComplete.as_view(), name='vehicle_model_auto_complete')
+
 ]
