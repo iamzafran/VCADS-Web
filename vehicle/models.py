ccba982f017616ce88dc6765aa277820a1412feb
zafranZS <muhammad.zafran@outlook.com>
2018-03-09 10:38:23 +0800

    deploy 17

--- vehicle/models.py
+++ vehicle/models.py
@@ -6,15 +6,29 @@ from django.db import models
 class VehicleMake(models.Model):
     vehicle_make = models.CharField(max_length=120)
 
-
-class VehicleModel(models.Model):
-    vehicle_model = models.CharField(max_length=120)
+    def __str__(self):
+        return self.vehicle_make
 
 
 class VehicleType(models.Model):
     vehicle_type = models.CharField(max_length=120)
 
+    def __str__(self):
+        return self.vehicle_type
+
+
+class VehicleModel(models.Model):
+    vehicle_model = models.CharField(max_length=120)
+    vehicle_type = models.ForeignKey(VehicleType, on_delete=models.SET_NULL, null=True)
+    vehicle_make = models.ForeignKey(VehicleMake, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.vehicle_model
+
 
 class Vehicle(models.Model):
-    vehicle_type = models.ForeignKey(VehicleType, on_delete=models.SET_NULL, null=True)
+    vehicle_model = models.ForeignKey(VehicleModel, on_delete=models.SET_NULL, null=True)
     license_plate = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.license_plate
