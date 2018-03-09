f91af6193027463f0cae6e7d605d38213bb34415
zafranZS <muhammad.zafran@outlook.com>
2018-03-09 12:31:32 +0800

    auto complete api

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='user_view')
+    url(r'^$', views.index, name='user_view'),
+    url(r'^api/model/autocomplete', name='vehicle_model_auto_complete')
 ]
--- vehicle/views.py
+++ vehicle/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http.response import HttpResponse
 
 # Create your views here.
+
+
 def index(request):
     return "user"
+
+
+class VehicleModelAutoComplete(APIView):
+
+    def get(self, request):
+        return HttpResponse("Vehicle Model AutoComplete")
